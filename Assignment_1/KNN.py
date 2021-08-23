ea61f5d428b71462ae5d23f213d27b1675adbc44
voljumet <alexander.laursen@icloud.com>
2021-08-23 18:16:16 +0200

    init + gitignore

---
+++ Assignment_1/KNN.py
@@ -0,0 +1,57 @@
+import numpy
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load pima indians dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.data.csv", delimiter=",")
+numpy.random.shuffle(dataset)
+splitratio = 0.8
+
+# split into input (X) and output (Y) variables
+X_train = dataset[:int(len(dataset)*splitratio),0:8]
+X_val = dataset[int(len(dataset)*splitratio):,0:8]
+Y_train = dataset[:int(len(dataset)*splitratio),8]
+Y_val = dataset[int(len(dataset)*splitratio):,8]
+print(X_train)
+print(Y_train)
+
+def distance(one,two):
+    return numpy.linalg.norm(one-two)
+
+def shortestDistance(x,x_rest,y_rest):
+    shortest = distance(x,x_rest[0])
+    predicted = y_rest[0]
+    for i in range(len(x_rest)):
+        if distance(x,x_rest[i])<=shortest:
+            shortest = distance(x,x_rest[i])
+            predicted = y_rest[i]
+    return predicted,shortest
+
+
+TP = 0
+TN = 0
+FP = 0
+FN = 0
+for i in range(len(X_val)):
+    x = X_val[i]
+    y = Y_val[i]
+    pred,shortest = shortestDistance(x,X_train,Y_train)
+    print("Y:",pred,"Y hat",y,"Distance:",shortest)
+
+    if(y==1 and pred ==1):
+        TP += 1
+
+    if(y==0 and pred ==0):
+        TN += 1
+
+    if(y==1 and pred ==0):
+        FN += 1
+
+    if(y==0 and pred ==1):
+        FP += 1
+
+print("Accuracy:",(TP+TN)/(TP+TN+FP+FN))
+print("Recall",TP/(TP+FN))
+print("Precision",TP/(TP+FP))
+print("F1",(2*TP)/(2*TP+FP+FN))
+
---
+++ Assignment_1/assignment_1.py
@@ -0,0 +1,88 @@
+import numpy
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load pima indians dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.data.csv", delimiter=",")
+numpy.random.shuffle(dataset)
+splitratio = 0.8
+
+# split into input (X) and output (Y) variables
+X_train = dataset[:int(len(dataset)*splitratio), 0:8]
+X_val = dataset[int(len(dataset)*splitratio):, 0:8]
+Y_train = dataset[:int(len(dataset)*splitratio), 8]
+Y_val = dataset[int(len(dataset)*splitratio):, 8]
+print(X_train)
+print(Y_train)
+k_value = 5
+
+
+def distance(one, two):
+    return numpy.linalg.norm(one - two)
+
+
+def shortest_distance(x, x_rest, y_rest):
+    shortest = distance(x, x_rest[0])
+    num_list_final = []
+    num_list_return = []
+    for i in range(len(x_rest)):
+        if distance(x, x_rest[i]) <= shortest:
+            num_list_final.append((distance(x, x_rest[i]), x_rest[i], y_rest[i]))
+    num_list_final.sort()
+
+    if len(num_list_final) >= k_value:
+        for k in range(k_value):
+            num_list_return.append(num_list_final[k])
+
+    elif len(num_list_final) < k_value:
+        intt = k_value - len(num_list_final)
+        print("intt: ", intt, ", len: ", len(num_list_final))
+        for u in range(len(num_list_final)):
+            num_list_return.append(num_list_final[u])
+
+        for w in range(intt-1):
+            num_list_return.append(num_list_final[w])
+
+    varr = 0
+    for f in range(len(num_list_return)):
+        varr += num_list_return[f][2]
+
+    if varr / k_value >= 0.5:
+        predicted = 1.0
+    else:
+        predicted = 0.0
+
+
+    return predicted, num_list_return
+
+
+
+TP = 0
+TN = 0
+FP = 0
+FN = 0
+
+for i in range(len(X_val)):
+    x = X_val[i]
+    y = Y_val[i]
+    pred, shortest = shortest_distance(x, X_train, Y_train)
+    print("Y:", pred, "Y hat", y)
+          # "Distance:", shortest)
+
+    if y == 1 and pred == 1:
+        TP += 1
+
+    if y == 0 and pred == 0:
+        TN += 1
+
+    if y == 1 and pred == 0:
+        FN += 1
+
+    if y == 0 and pred == 1:
+        FP += 1
+
+print("Accuracy:", (TP+TN)/(TP+TN+FP+FN))
+print("Recall", TP/(TP+FN))
+print("Precision", TP/(TP+FP))
+print("F1", (2*TP)/(2*TP+FP+FN))
+
